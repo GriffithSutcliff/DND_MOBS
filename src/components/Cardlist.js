03ffc0b35afa7d98737794a869cb1b9ded7f99a1
GriffithSutcliff <darkborn56@gmail.com>
2024-06-13 13:15:39 +0300

    search

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -5,6 +5,7 @@ export default function Cardlist({ onMonsterClick }) {
   const [monsters, setMonsters] = useState([]);
   const [error, setError] = useState(null);
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
   const monstersPerPage = 12;
 
   useEffect(() => {
@@ -36,17 +37,32 @@ export default function Cardlist({ onMonsterClick }) {
     setCurrentSlide(currentSlide === 0 ? totalSlides - 1 : currentSlide - 1);
   };
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-  const renderMonsters = monsters.slice(
+  const filteredMonsters = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const renderMonsters = filteredMonsters.slice(
     currentSlide * monstersPerPage,
     currentSlide * monstersPerPage + monstersPerPage
   );
 
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className='front'>
+        <input
+          className='search'
+          type="text"
+          placeholder="Search by name"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
       <div className="monster-list">
         {renderMonsters.map(monster => (
           <Card key={monster.index} monster={monster} onCardClick={onMonsterClick} />
